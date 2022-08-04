734b24897b68c1906d6db786e67c04981943602b
arnold-oghiator <arnoldoghiator@gmail.com>
2022-08-04 10:27:44 +0100

    checkbox functionality

---
+++ Checkbox/index.js
@@ -0,0 +1,19 @@
+const inputBoxes = document.querySelectorAll('.input input[type="checkbox"]')
+console.log(inputBoxes)
+
+let lastChecked;
+function handleClick(e){
+
+    console.log(e)
+    let inBetween = false
+    if(e.shiftKey && this.checked){
+        inputBoxes.forEach((item) =>{
+if(item ==this || item === lastChecked){
+    inBetween = !inBetween
+}
+        })
+    }
+lastChecked = this
+}
+
+inputBoxes.forEach((item)=>item.addEventListener('click', handleClick))
