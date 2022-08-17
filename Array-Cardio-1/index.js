345b7f30af1e88959b100d00b072edaa290094ad
arnold-oghiator <arnoldoghiator@gmail.com>
2022-08-17 21:10:11 +0100

    adding css variable

--- Array-Cardio-1/index.js
+++ Array-Cardio-1/index.js
@@ -164,14 +164,6 @@ console.log(surname)
 
 
 
-
-
-
-
-
-
-
-
 const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck', 'pogostick']
 
 
