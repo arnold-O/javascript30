986c008a8f157012b40cd5fa32e0ee3ebc5616d1
arnold-O <arnoldoghiator@gmail.com>
2023-01-07 11:27:26 +0100

    algorithm

---
+++ Algorithm/Algorithm.js
@@ -0,0 +1,21 @@
+//////////******ARRAYS ********/////////
+
+
+
+const arr = [1, 2, 3, 'hismab']
+
+
+// for(const item of arr){
+    // }
+    arr.splice(2, 1)
+    
+        console.log(arr)
+
+
+
+        // for Arrays: splice, slice, filter, reduce, concat, sort, find, map, set.
+        //For objects:  keys, entries, values,
+//////////******ARRAYS ********/////////
+
+
+
