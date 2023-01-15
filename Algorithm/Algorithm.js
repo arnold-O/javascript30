a2384f20b01ade912da0752afe4c61caae350c03
arnold-O <arnoldoghiator@gmail.com>
2023-01-15 09:44:28 +0100

    string reversal

--- Algorithm/Algorithm.js
+++ Algorithm/Algorithm.js
@@ -2,20 +2,26 @@
 
 
 
-const arr = [1, 2, 3, 'hismab']
+// const arr = [1, 2, 3, 'hismab']
 
 
-// for(const item of arr){
-    // }
-    arr.splice(2, 1)
+// // for(const item of arr){
+//     // }
+//     arr.splice(2, 1)
     
-        console.log(arr)
+//         console.log(arr)
 
 
 
-        // for Arrays: splice, slice, filter, reduce, concat, sort, find, map, set.
-        //For objects:  keys, entries, values,
-//////////******ARRAYS ********/////////
+//         // for Arrays: splice, slice, filter, reduce, concat, sort, find, map, set.
+//         //For objects:  keys, entries, values,
+// //////////******ARRAYS ********/////////
+function strFunction(str){
+    return str.split('').reduce((total, curr)=>{
+        return curr + total;
 
-
+    }, '')
+}
+const valueInput = strFunction('Arnold')
+console.log(valueInput)
 
