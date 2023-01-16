76128385eae5fb326ce5801355a6ed43fe65ac55
arnold-O <arnoldoghiator@gmail.com>
2023-01-16 22:59:07 +0100

    palindromw

--- Algorithm/Algorithm.js
+++ Algorithm/Algorithm.js
@@ -1,27 +1,38 @@
 //////////******ARRAYS ********/////////
 
-
-
 // const arr = [1, 2, 3, 'hismab']
-
 
 // // for(const item of arr){
 //     // }
 //     arr.splice(2, 1)
-    
+
 //         console.log(arr)
-
-
 
 //         // for Arrays: splice, slice, filter, reduce, concat, sort, find, map, set.
 //         //For objects:  keys, entries, values,
 // //////////******ARRAYS ********/////////
-function strFunction(str){
-    return str.split('').reduce((total, curr)=>{
-        return curr + total;
-
-    }, '')
+function strFunction(str) {
+  return str.split("").reduce((total, curr) => {
+    return curr + total;
+  }, "");
 }
-const valueInput = strFunction('Arnold')
-console.log(valueInput)
+const valueInput = strFunction("Arnold");
+console.log(valueInput);
 
+// PALINDROMES
+
+const palindrome = (params) => {
+  // const reversed = params.split("").reduce((total, curr) => {
+  //     return curr + total;
+  //   }, "");
+  let initial = "";
+
+  for (let val of params) {
+    initial = val + initial;
+  }
+
+  return initial === params;
+};
+console.log(palindrome("preerp"));
+
+// Interger Reversal
