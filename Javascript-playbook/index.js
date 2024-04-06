1d4c894c3f33de0aee9aede4f5f2a1308729a63c
arnold-O <arnoldoghiator@gmail.com>
2024-04-06 02:00:32 +0100

    reviews project

--- Javascript-playbook/index.js
+++ Javascript-playbook/index.js
@@ -28,14 +28,67 @@
 
 
 
-const btn = document.querySelector('.btn');
-const header1 = document.querySelector('p');
+// const btn = document.querySelector('.btn');
+// const header1 = document.querySelector('p');
 
-header1.addEventListener(('click'), function(e){
-    this.classList.add('blue')
-    console.log(e.currentTarget)
-})
-btn.addEventListener(('click'), function(e){
+// header1.addEventListener(('click'), function(e){
+//     this.classList.add('blue')
+//     console.log(e.currentTarget)
+// })
+// btn.addEventListener(('click'), function(e){
   
-    console.log(e.currentTarget)
+//     console.log(e.currentTarget)
+// })
+
+// const container = document.querySelector('.container');
+// const listItem = document.querySelector('.list-item');
+
+// container.addEventListener('click',(e)=>{
+   
+//         console.log(e.target)
+//         console.log(e.currentTarget)
+    
+// })
+
+const friends =["what your name", 'Going anywhere', 'today na oday']
+
+localStorage.setItem("name", JSON.stringify(friends))
+
+const itemget = JSON.parse(localStorage.getItem("name"))
+
+
+
+const btns = document.querySelectorAll('.btn');
+const counterHtml = document.querySelector('.counter');
+let counter = 0;
+btns.forEach(function(btn){
+    btn.addEventListener('click', function(e){
+    const check = e.currentTarget.classList;
+        if(check.contains("decrease")){
+          counter--
+        }
+        else if(check.contains("increase")){
+            counter++
+        }
+        else{
+            counter = 0
+        }
+counterHtml.textContent = counter;
 })
+})
+
+
+/**++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 
+ * 
+ 
+ * 
+*/
+
+
+const  reviewsdata =  [
+    {
+        id:1,
+        name:"susan peters",
+        img:"https://media.istockphoto.com/id/1437816897/photo/business-woman-manager-or-human-resources-portrait-for-career-success-company-we-are-hiring.jpg?s=612x612&w=0&k=20&c=tyLvtzutRh22j9GqSGI33Z4HpIwv9vL_MZw_xOE19NQ="
+    }
+]
