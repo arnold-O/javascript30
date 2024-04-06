1828d936fe4b9d5a288cf1b31b2a6606d496ff83
arnold-O <arnoldoghiator@gmail.com>
2024-04-06 08:07:30 +0100

    addind more json data

--- Javascript-playbook/index.js
+++ Javascript-playbook/index.js
@@ -98,19 +98,37 @@ const  reviewsdata =  [
         id:1,
         name:"Gold Sachs",
         job:"Content Creator",
-        img:"https://media.istockphoto.com/id/1437816897/photo/business-woman-manager-or-human-resources-portrait-for-career-success-company-we-are-hiring.jpg?s=612x612&w=0&k=20&c=tyLvtzutRh22j9GqSGI33Z4HpIwv9vL_MZw_xOE19NQ="
+        img:"https://media.istockphoto.com/id/1437816897/photo/business-woman-manager-or-human-resources-portrait-for-career-success-company-we-are-hiring.jpg?s=612x612&w=0&k=20&c=tyLvtzutRh22j9GqSGI33Z4HpIwv9vL_MZw_xOE19NQ=",
+        text:"o let us embrace the serendipity of randomness, for within its chaos lies the beauty of discovery. And who knows? Maybe in these seemingly."
     },
     {
         id:2,
         name:"susan peters",
         job:"System Analyst",
-        img:"https://media.istockphoto.com/id/1398385367/photo/happy-millennial-business-woman-in-glasses-posing-with-hands-folded.jpg?s=612x612&w=0&k=20&c=Wd2vTDd6tJ5SeEY-aw0WL0bew8TAkyUGVvNQRj3oJFw="
+        img:"https://media.istockphoto.com/id/1398385367/photo/happy-millennial-business-woman-in-glasses-posing-with-hands-folded.jpg?s=612x612&w=0&k=20&c=Wd2vTDd6tJ5SeEY-aw0WL0bew8TAkyUGVvNQRj3oJFw=",
+        text:"a cosmic crossword where syllables intersect, creating riddles for our contemplation. Each sentence, a constellation of possibility, invites."
     },
     {
         id:3,
         name:"Ken Sarowiwa",
         job:"Developer",
-        img:"https://media.istockphoto.com/id/1399788030/photo/portrait-of-young-confident-indian-woman-pose-on-background.jpg?s=612x612&w=0&k=20&c=aQw5YhGl99hL1O77thwpQTmqVE7bc8rCX9H0gTeoX_k="
+        img:"https://media.istockphoto.com/id/1399788030/photo/portrait-of-young-confident-indian-woman-pose-on-background.jpg?s=612x612&w=0&k=20&c=aQw5YhGl99hL1O77thwpQTmqVE7bc8rCX9H0gTeoX_k=",
+        text:"a serendipitous dance of letters that pirouettes across the page. It flits between meaning and ambiguity, teasing our minds with its elusive charm."
+    },
+    {
+        id:4,
+        name:"Greden Sharoro",
+        job:"Director",
+        img:"https://media.istockphoto.com/id/1338134336/photo/headshot-portrait-african-30s-man-smile-look-at-camera.jpg?s=612x612&w=0&k=20&c=kUVdvBnwnZRxausswIKRZuC25bZgTXwrmGSPikdcOro=",
+        text:"a serendipitous dance of letters that pirouettes across the page. It flits between meaning and ambiguity, teasing our minds with its elusive charm."
+    },
+
+    {
+        id:5,
+        name:"skies Tyler",
+        job:"Photographer",
+        img:"https://media.istockphoto.com/id/1351285381/photo/happy-young-african-american-woman-looking-at-camera.jpg?s=612x612&w=0&k=20&c=e6sawVkyC7t_NTsFb13RWnLq4b9CYon4TJODpS2WU4I=",
+        text:"a serendipitous dance of letters that pirouettes across the page. It flits between meaning and ambiguity, teasing our minds with its elusive charm."
     }
 ]
 let count = 0;
@@ -118,6 +136,7 @@ function utils(count){
     namer.textContent = reviewsdata[count].name;
     job.textContent = reviewsdata[count].job;
     img.setAttribute('src', reviewsdata[count].img ) ;
+    text.textContent = reviewsdata[count].text
 }
 prev.addEventListener('click', function(e){
 
